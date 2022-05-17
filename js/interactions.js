45f5007647260fd4f651634a06398d4608158ce9
SAX_94 <matteosacchi94@gmail.com>
2022-05-17 21:36:05 +0200

    interactions.JS with questions created

---
+++ js/interactions.js
@@ -0,0 +1,6 @@
+const firstName = prompt("What's your first name?");
+const lastName = prompt("What's your last name?");
+const favoriteColor = prompt("What's your favorite color?");
+
+/* Alert message with generated password */
+alert(`Your password is: ${firstName}${lastName}${favoriteColor}22`);
