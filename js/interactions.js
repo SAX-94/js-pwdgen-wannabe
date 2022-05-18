510b9eca3a69b6c889e90bbb129f6216eca3330e
SAX_94 <matteosacchi94@gmail.com>
2022-05-18 09:22:53 +0200

    finished

--- js/interactions.js
+++ js/interactions.js
@@ -2,5 +2,10 @@ const firstName = prompt("What's your first name?");
 const lastName = prompt("What's your last name?");
 const favoriteColor = prompt("What's your favorite color?");
 
-/* Alert message with generated password */
-alert(`Your password is: ${firstName}${lastName}${favoriteColor}22`);
+const password = `${firstName}${lastName}${favoriteColor}22`
+
+const spanpassword = document.getElementById("span-password")
+
+
+spanpassword.innerHTML = `${password}`;
+console.dir(spanpassword);
