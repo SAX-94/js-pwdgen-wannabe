8e429a2730a08765838dac8d7cbd7ed230c9e5de
SAX_94 <matteosacchi94@gmail.com>
2022-05-18 09:46:45 +0200

    added forgotten semicolons

--- js/interactions.js
+++ js/interactions.js
@@ -2,9 +2,9 @@ const firstName = prompt("What's your first name?");
 const lastName = prompt("What's your last name?");
 const favoriteColor = prompt("What's your favorite color?");
 
-const password = `${firstName}${lastName}${favoriteColor}22`
+const password = `${firstName}${lastName}${favoriteColor}22`;
 
-const spanpassword = document.getElementById("span-password")
+const spanpassword = document.getElementById("span-password");
 
 
 spanpassword.innerHTML = `${password}`;
